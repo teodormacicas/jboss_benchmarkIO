e9a550774d7f4d7034f8acbe0de21e1811793140
Nabil Benothman <nabil.benothman@gmail.com>
2011-11-18 11:24:39 +0100

    Write big file to client

--- src/main/java/org/jboss/nio2/server/async/CompletionHandlerImpl.java
+++ src/main/java/org/jboss/nio2/server/async/CompletionHandlerImpl.java
@@ -23,13 +23,14 @@ package org.jboss.nio2.server.async;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousSocketChannel;
 import java.nio.channels.CompletionHandler;
-import java.nio.channels.FileChannel;
+import java.nio.channels.SeekableByteChannel;
 import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.concurrent.Future;
 
 import org.jboss.logging.Logger;
@@ -118,8 +119,34 @@ class CompletionHandlerImpl implements CompletionHandler<Integer, AsynchronousSo
 	 */
 	protected void writeResponse(AsynchronousSocketChannel channel) throws Exception {
 
-		System.out.println("\t-> Start writeResponse");
+		System.out.println("-> Start writeResponse");
 
+		File file = new File("data" + File.separatorChar + "file.txt");
+		Path path = FileSystems.getDefault().getPath(file.getAbsolutePath());
+		SeekableByteChannel sbc = null;
+		ByteBuffer writeBuffer = ByteBuffer.allocate(8 * 1024);
+		try {
+			sbc = Files.newByteChannel(path, StandardOpenOption.READ);
+			// Read from file and write to the asynchronous socket channel
+			while (sbc.read(writeBuffer) > 0) {
+				write(channel, writeBuffer);
+			}
+
+			// write the CRLF characters
+			writeBuffer.put(CRLF.getBytes());
+			write(channel, writeBuffer);
+		} catch (Exception exp) {
+			logger.error("Exception: " + exp.getMessage(), exp);
+			exp.printStackTrace();
+		} finally {
+			if (sbc != null) {
+				sbc.close();
+			}
+		}
+		
+		
+		
+		/*
 		File file = new File("data" + File.separatorChar + "file.txt");
 		Path path = FileSystems.getDefault().getPath(file.getAbsolutePath());
 		// SeekableByteChannel sbc = null;
@@ -153,7 +180,8 @@ class CompletionHandlerImpl implements CompletionHandler<Integer, AsynchronousSo
 			fileChannel.close();
 			raf.close();
 		}
-		System.out.println("\t-> End writeResponse");
+		*/
+		System.out.println("-> End writeResponse");
 	}
 
 	/**
@@ -165,7 +193,7 @@ class CompletionHandlerImpl implements CompletionHandler<Integer, AsynchronousSo
 	 */
 	protected void write(AsynchronousSocketChannel channel, ByteBuffer[] buffers) throws Exception {
 
-		System.out.println("\t\t-> Start write");
+		System.out.println("\t-> Start write");
 
 		for (int i = 0; i < buffers.length; i++) {
 			buffers[i].flip();
@@ -184,7 +212,7 @@ class CompletionHandlerImpl implements CompletionHandler<Integer, AsynchronousSo
 						exc.printStackTrace();
 					}
 				});
-		System.out.println("\t\t-> End write");
+		System.out.println("\t-> End write");
 
 	}
 
