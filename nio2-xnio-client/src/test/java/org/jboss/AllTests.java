de1e61e0924493ab0253a5a7d4a5acdc9ca19d30
Aliya Ibragimova <aliya.ibragimova@unifr.ch>
2013-04-21 19:34:07 +0200

    Add property file to tests

--- nio2-xnio-client/src/test/java/org/jboss/AllTests.java
+++ nio2-xnio-client/src/test/java/org/jboss/AllTests.java
@@ -1,13 +1,30 @@
 package org.jboss;
 
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+
+import java.io.IOException;
+import java.util.Properties;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({ ClientDelayTest.class })
 public class AllTests {
-    public static final String HOSTNAME = "localhost";
-    public static final int PORT = 8081;
-    public static final int NUM_THREADS = 10;
-    public static final int NUM_REQUESTS = 100;
+
+    public static String HOSTNAME;
+    public static int PORT;
+    public static int NUM_THREADS;
+    public static int NUM_REQUESTS;
+
+    @BeforeClass
+    public static void setupAllTests() throws IOException{
+
+        Properties props = new Properties();
+        props.load(ClientDelayTest.class.getClassLoader().getResourceAsStream("test.properties"));
+
+        HOSTNAME = props.getProperty("hostname");
+        PORT = Integer.valueOf(props.getProperty("port"));
+        NUM_THREADS = Integer.valueOf(props.getProperty("numthreads"));
+        NUM_REQUESTS = Integer.valueOf(props.getProperty("numrequests"));
+    }
 }
